329c2ccdb7262adedb0e2e6868fc38889a817b8d
Bookmarkeltman <peanut20230@gmail.com>
2025-08-05 09:06:29 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Basic side menu toggle functionality
+function toggleMenu() {
+  const menu = document.getElementById('side-menu');
+  menu.classList.toggle('hidden');
+}
+
+// (Optional) You can later add Google sign-in/sign-up integration here
